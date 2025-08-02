053ce82e9dcd8dcb25b9e210cf5cde28d2abc7ad
regis-mugisha <mugisharegis72@gmail.com>
2025-08-02 19:33:58 +0200

    feat: add PageLoader component for indicating loading process

---
+++ components/PageLoader.jsx
@@ -0,0 +1,13 @@
+import { View, ActivityIndicator } from "react-native";
+import { COLORS } from "../constants/colors";
+import { styles } from "../styles/home.styles";
+
+const PageLoader = () => {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color={COLORS.primary} />
+    </View>
+  );
+};
+
+export default PageLoader;
